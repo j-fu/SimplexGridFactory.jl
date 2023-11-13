dc47eaea3788ea35cde8f15bc67c3a3d93e618b2
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2023-11-13 23:43:36 +0100

    qualified imports

--- src/SimplexGridFactory.jl
+++ src/SimplexGridFactory.jl
@@ -1,26 +1,27 @@
 module SimplexGridFactory
 
-using Printf
-using LinearAlgebra
+using Printf: @printf, @sprintf
+using LinearAlgebra: norm
 
-using ExtendableGrids
-using GridVisualize
-using DocStringExtensions
-using ElasticArrays
-using FileIO
-using MeshIO
+using ElasticArrays: ElasticArray
+import ExtendableGrids
+using ExtendableGrids: dim_space, Coordinates, BFaceNodes, BFaceRegions, CellNodes, CellRegions
+using GridVisualize: PyPlotType, plottertype, MakieType, GridVisualizer
+using DocStringExtensions: SIGNATURES, TYPEDEF, TYPEDSIGNATURES
+using FileIO: load
+import MeshIO
 
 include("utils.jl")
 
 include("options.jl")
 
 include("binnedpointlist.jl")
-export  BinnedPointList
+export BinnedPointList
 
 include("simplexgridbuilder.jl")
 export SimplexGridBuilder
-export point!,facet!,polyfacet!, cellregion!, holepoint!,facetregion!,maxvolume!,regionpoint!,options!
-export istriangulate,istetgen,maybewatertight
+export point!, facet!, polyfacet!, cellregion!, holepoint!, facetregion!, maxvolume!, regionpoint!, options!
+export istriangulate, istetgen, maybewatertight
 export flags
 
 include("simplexgrid.jl")
@@ -31,7 +32,7 @@ include("triangle.jl")
 include("tetgen.jl")
 
 include("primitives.jl")
-export circle!,rect2d!,rect3d!,sphere!,bregions!,moveto!,lineto!
+export circle!, rect2d!, rect3d!, sphere!, bregions!, moveto!, lineto!
 export model3d!, mesh3d!
 
 include("plot.jl")
